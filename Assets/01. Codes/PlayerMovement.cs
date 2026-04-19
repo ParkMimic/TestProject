ad0815ceaa37a6ac288af2346341c379fb137fc0
ParkMimic <mimics626@gmail.com>
2026-04-19 18:46:24 +0900

    20260419_Park
    플레이어 움직임
    flip 추가
    tilemap 머지 하는 법 공부

--- Assets/01. Codes/PlayerMovement.cs
+++ Assets/01. Codes/PlayerMovement.cs
@@ -6,17 +6,45 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     Rigidbody2D rigid;
+    SpriteRenderer sprite;
     Animator anim;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
+        float inputX = Input.GetAxisRaw("Horizontal");
+        
+        Movement(inputX);
+
+        if (!Input.GetButton("Horizontal")) return;
+        Flip(inputX);
+    }
+
+    void Movement(float inputX)
+    {
         rigid.linearVelocityX = inputX * moveSpeed;
+
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.linearVelocityX = 0;
+        }
+    }
+
+    void Flip(float inputX)
+    {
+        if (inputX > 0)
+        {
+            sprite.flipX = false;
+        }
+        else
+        {
+            sprite.flipX = true;
+        }
     }
 }
